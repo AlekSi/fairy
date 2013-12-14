4e6b03a0129ceb844e9523632a3bf5f55ecc8883
Markov Alexey <piton4eg@mail.ru>
2013-12-14 18:06:18 +0400

    Add filter

--- public/js/fairy.js
+++ public/js/fairy.js
@@ -2,11 +2,16 @@
 
 function MessageCtrl($scope, $http, $timeout) {
   $scope.messages = [];
+  $scope.count = 0;
 
   (function tick() {
     $http.get("/topic").
       success(function(data, status, headers, config) {
         $scope.messages.push({body: data});
+        if ($scope.count > 20) {
+          $scope.messages.splice(0, 1);
+        }
+        $scope.count = $scope.count + 1;
         $timeout(tick, 0);
       }).
       error(function(data, status, headers, config) {
