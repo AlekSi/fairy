e9148e2f28124831e3cd4c59b75ea89ea1cec89f
kisakov <isakov90@gmail.com>
2013-12-14 12:30:10 +0400

    init

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"html"
+	"log"
+	"net/http"
 )
 
+func handler(rw http.ResponseWriter, req *http.Request) {
+	topic := req.URL.Path
+  switch req.Method {
+    rw.WriteHeader(201)
+  }
+
+}
+
 func main() {
-	fmt.Println("Hello world!")
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
